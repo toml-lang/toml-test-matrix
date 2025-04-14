7d8b5757a5c6b0baaef91539cd8736d524ce1a59
Ed Page <eopage@gmail.com>
2025-04-14 11:33:21 -0500

    Re-add rust-boml

--- parsers/rust-boml.zsh
+++ parsers/rust-boml.zsh
@@ -6,13 +6,12 @@ setup() {
 }
 
 typeset -A info=(
-	skip    1
 	lang    'Rust'
 	toml    '1.0'
 	site    'https://github.com/Bright-Shard/boml'
 	src     '' # We use the Cargo.toml for now 'https://github.com/toml-rs/toml.git'
 	version '0.3.1'
 	decoder './scripts/target/release/boml-decoder'
-	encoder './scripts/target/release/boml-encoder'
+	encoder ''
 	perf    './scripts/target/release/rust-boml-perf'
 )
